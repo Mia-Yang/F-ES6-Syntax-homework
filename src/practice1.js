82ab323c5a4a7f056f025afcb4af11ac833daf3e
Yang Siyu <siyuyang1995@gmail.com>
2020-09-13 22:53:27 +0800

    [yang siyu]feat: pass the practice 1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,17 @@
-const parseData
+const parseData = (input) => {
+    let keys = [];
+    for (let col of input.column) {
+        keys.push(col.name);
+    }
+
+    let output = [];
+    for (let row of input.data) {
+        let obj = {};
+        for (let i in row) {
+            obj[keys[i]] = row[i];
+        }
+        output.push(obj);
+    }
+    return output;
+}
 export { parseData };
