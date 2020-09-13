0f402759be45bc2792d8bb44cd1d27827868a0a4
Yang Siyu <siyuyang1995@gmail.com>
2020-09-13 23:10:55 +0800

    [yang siyu]feat: pass the practice2

--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+    sections.sort((x, y) => {return y.index - x.index})
+    for (let row of sections) {
+        let {content, index} = row;
+        items.splice(index, 0, content);
+    }
+    return items;
+}
 export { inject };
