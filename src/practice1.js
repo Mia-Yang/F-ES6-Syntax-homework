836e17d3ebfa9593ffd5e7bb9572df1a5eb6b34d
kevindongzg <dongkevin94@gmail.com>
2020-09-22 19:55:49 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,9 +1,13 @@
+// TODO 可以思考一下可否用reduce来做
+// TODO 使用解构的方式取出data与column
 const parseData = (input) => {
+    // TODO keys没有太大必要
     let keys = [];
     for (let col of input.column) {
         keys.push(col.name);
     }
 
+    // TODO 使用map做数组与数组的转化，不用声明新数组
     let output = [];
     for (let row of input.data) {
         let obj = {};
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,6 @@
+// TODO 挺不错的，思路正确简洁，且没有逻辑纰漏
 const inject = (items, sections) => {
+    // TODO 这里箭头函数return可以简写
     sections.sort((x, y) => {return y.index - x.index})
     for (let row of sections) {
         let {content, index} = row;
